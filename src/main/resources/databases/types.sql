4b3b05afe3ebebb5a6dbcf61636f14fb12a968b0
danlevik <leviol80@gmail.com>
2022-05-09 15:26:33 +0300

    -added types.sql

---
+++ src/main/resources/databases/types.sql
@@ -0,0 +1,5 @@
+INSERT INTO types (type_name) VALUES ('Печенье');
+INSERT INTO types (type_name) VALUES ('Пирожные');
+INSERT INTO types (type_name) VALUES ('Пончики');
+INSERT INTO types (type_name) VALUES ('Торты');
+INSERT INTO types (type_name) VALUES ('Конфеты')
